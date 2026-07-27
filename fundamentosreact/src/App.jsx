1e56bfbe1c402069f82dd20c3dbeaee4c509f0a4
Karen Prieto <lizethprieto38@gmail.com>
2026-07-27 12:08:03 -0500

    Hoja de vida react

--- fundamentosreact/src/App.jsx
+++ fundamentosreact/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import foto from './assets/foto.jpg'
 import './App.css'
 
 function App() {
@@ -9,112 +7,23 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Bienvenidos</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div>
+        <h1>HOJA DE VIDA APRENDIZ</h1>
+        <h2>ANALISIS Y DESARROLLO DE SOFTWARE</h2>
+        <p>Karen Lizeth Prieto</p>
+        <img src={foto} alt="foto de perfil" width="250"/>
+        <h3>INFORMACIÓN PERSONAL:</h3>
+        <p>Karen Lizeth Prieto Mendivelso</p>
+        <p>18 años</p>
+        <p>Bogotá</p>
+        <p>lizethprieto38@gmail.com</p>
+        <p>Analisis y desarrollo de software</p>
+        <h3>CURSOS REALIZADOS:</h3>
+        <p>Curso de estilismo</p>
+        <h3>OBJETIVO PROFESIONAL</h3>
+        <p>Deseo aprender React para fortalecer mis habilidades en el desarrollo frontend y crear aplicaciones web modernas, dinámicas y escalables. Me gustaría aplicar estos conocimientos en proyectos de plataformas empresariales, comercio electrónico y aplicaciones que mejoren la experiencia de los usuarios.</p>
+        <button>Conoce más sobre mí</button>
+      </div>
     </>
   )
 }
